1a252786e070868d369f8bb42583f65b0529812c
judithquine <judithquine@yahoo.es>
2021-11-03 14:11:34 +0100

    add main.js

--- main.js
+++ main.js
@@ -1,3 +1,23 @@
 $(document).ready(function() {
-	console.log("Hola!");
-})
+	let tasks = [];
+	$('#submit').on("click", function() {
+		let newTask = $('#newTask').val();
+		
+		if (newTask !== "") {
+			tasks.push(newTask);
+	
+			$('#taskList').empty()
+        
+			tasks.forEach( t => 
+    			$('#taskList').append(
+        		`<div class="task">${t}</div>`
+    			)
+			);
+		}		
+	})
+	$('#clearButton').on("click", function () {
+		$('.task').remove();
+		tasks = [];
+    })	
+
+});
